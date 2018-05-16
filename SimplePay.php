777d530a02eaed0086b1c3cd7e4332ca564bc590
Gyula Szabó <gt.szgyula@gmail.com>
2018-05-17 01:43:31 +0200

    Make component more flexible
    The SDK config array can now be extended instead of overwritten.
    The backref processing function defaults to query parameters.
    The callback URL generation scheme can now be used from outside.

--- SimplePay.php
+++ SimplePay.php
@@ -2,6 +2,7 @@
 
 namespace h3tech\simplePay;
 
+use Yii;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
 
@@ -33,6 +34,11 @@ class SimplePay extends Component
         return $this->sdkConfig;
     }
 
+    public function generateCallbackUrl($routeDefinition)
+    {
+        return preg_replace('/^\/{2}/', '', Url::to($routeDefinition, ''));
+    }
+
     /**
      * @param $config
      * @throws InvalidConfigException
@@ -45,7 +51,7 @@ class SimplePay extends Component
 
         foreach ($config as $key => $value) {
             if (($key === 'BACK_REF' || preg_match('/^[A-Z_]+_URL$/', $key)) && is_array($value)) {
-                $config[$key] = preg_replace('/^\/{2}/', '', Url::to($value, ''));
+                $config[$key] = $this->generateCallbackUrl($value);
             }
         }
 
@@ -98,39 +104,48 @@ class SimplePay extends Component
         $this->defaultPaymentMethod = $paymentMethod;
     }
 
+    protected function generateConfigArray($config = null)
+    {
+        return is_array($config) ? array_merge($this->sdkConfig, $config) : $this->sdkConfig;
+    }
+
     public function createLiveUpdate($currency = '', array $config = null)
     {
-        return new SimpleLiveUpdate(is_array($config) ? $config : $this->sdkConfig, $currency);
+        return new SimpleLiveUpdate($this->generateConfigArray($config), $currency);
     }
 
     public function createBackRef($currency = '', array $config = null)
     {
-        return new SimpleBackRef(is_array($config) ? $config : $this->sdkConfig, $currency);
+        return new SimpleBackRef($this->generateConfigArray($config), $currency);
     }
 
     public function createIos($currency = '', $orderNumber = 'N/A', array $config = null)
     {
-        return new SimpleIos(is_array($config) ? $config : $this->sdkConfig, $currency, $orderNumber);
+        return new SimpleIos($this->generateConfigArray($config), $currency, $orderNumber);
     }
 
     public function createIpn($currency = '', array $config = null)
     {
-        return new SimpleIpn(is_array($config) ? $config : $this->sdkConfig, $currency);
+        return new SimpleIpn($this->generateConfigArray($config), $currency);
     }
 
     public function createIdn($currency = '', array $config = null)
     {
-        return new SimpleIdn(is_array($config) ? $config : $this->sdkConfig, $currency);
+        return new SimpleIdn($this->generateConfigArray($config), $currency);
     }
 
     public function createIrn($currency = '', array $config = null)
     {
-        return new SimpleIrn(is_array($config) ? $config : $this->sdkConfig, $currency);
+        return new SimpleIrn($this->generateConfigArray($config), $currency);
     }
 
     /** @return string|array */
-    public function processBackRef(array $parameters)
+    public function processBackRef(array $parameters = null)
     {
+        if ($parameters === null) {
+            $parameters = Yii::$app->request->queryParams;
+        }
+
         $orderCurrency = isset($parameters['order_currency']) ? $parameters['order_currency'] : 'N/A';
         $backref = $this->createBackRef($orderCurrency);
         $backref->order_ref = isset($parameters['order_ref']) ? $parameters['order_ref'] : 'N/A';
